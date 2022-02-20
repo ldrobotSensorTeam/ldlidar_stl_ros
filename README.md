28430bc2f4547e578df5fbd386598203c57c5bcd
DavidHu <hmd_hubei_cn@163.com>
2022-02-20 22:07:14 +0800

    [add][ld06,ld19]内部开发ROS包完成，对外开源，初始创建库

---
+++ README.md
@@ -0,0 +1,155 @@
+- [cn](#操作指南)
+- [en](#Instructions)
+# 操作指南
+
+>此SDK仅适用于深圳乐动机器人有限公司销售的激光雷达产品，产品型号为:
+> - LDROBOT LiDAR LD06
+> - LDROBOT LiDAR LD19
+## 0. 获取雷达的ROS功能包
+```bash
+$ cd ~
+
+$ mkdir -p ldlidar_ros_ws/src
+
+$ cd ldlidar_ros_ws/src
+
+$ git clone  https://github.com/ldrobotSensorTeam/ldlidar_stl_ros.git
+# 或者
+$ git clone  https://gitee.com/ldrobotSensorTeam/ldlidar_stl_ros.git
+```
+
+## 1. 系统设置
+- 第一步，通过板载串口或者USB转串口模块(例如,cp2102模块)的方式使雷达连接到你的系统主板.
+- 第二步，设置雷达在系统中挂载的串口设备-x权限(以/dev/ttyUSB0为例)
+	- 实际使用时，根据雷达在你的系统中的实际挂载情况来设置，可以使用`ls -l /dev`命令查看.
+
+``` bash
+$ cd ~/ldlidar_ros_ws
+
+$ sudo chmod 777 /dev/ttyUSB0
+```
+- 第三步，修改`launch/`目录下雷达产品型号对应的lanuch文件中的`port_name`值，以ld06.launch为例，如下所示.
+
+```xml
+<launch>
+ <node name="LD06" pkg="ldlidar_stl_ros" type="ldlidar_stl_ros_node" output="screen" >
+ <param name="product_name" value="LDLiDAR_LD06"/>
+ <param name="topic_name" value="LiDAR/LD06"/>
+ <param name="port_name" value ="/dev/ttyUSB0"/>
+ <param name="frame_id" value="lidar_frame"/>
+ </node>
+</launch>
+```
+## 2. 编译方法
+
+使用catkin编译，在readme文件所在目录下执行如下指令.
+
+```bash
+catkin_make
+```
+## 3. 运行方法
+
+```bash
+source devel/setup.bash
+```
+- 产品型号为 LDROBOT LiDAR LD06
+
+  ``` bash
+  roslaunch ldlidar_stl_ros ld06.launch
+  ```
+- 产品型号为 LDROBOT LiDAR LD19
+
+  ``` bash
+  roslaunch ldlidar_stl_ros ld19.launch
+  ```
+##   4. 测试
+
+> 代码支持ubuntu16.04 ROS kinetic、ubuntu18.04 ROS melodic、ubuntu20.04 ROS noetic版本下测试，使用rviz可视化。
+
+- 新打开一个终端 (Ctrl + Alt + T),并通过Rviz工具打开readme文件所在目录的rviz文件夹下面的ldlidar.rviz文件
+```bash
+rosrun rviz rviz
+```
+
+| 产品型号:          | Fixed Frame: | Topic:        |
+| ------------------ | ------------ | ------------- |
+| LDROBOT LiDAR LD06 | lidar_frame  | /LiDAR/LD06 |
+| LDROBOT LiDAR LD19 | lidar_frame  | /LiDAR/LD19 |
+
+
+# Instructions
+
+> This SDK is only applicable to the LiDAR products sold by Shenzhen LDROBOT Co., LTD. The product models are :
+> - LDROBOT LiDAR LD06
+> - LDROBOT LiDAR LD19
+## 0. get LiDAR ROS Package
+```bash
+$ cd ~
+
+$ mkdir -p ldlidar_ros_ws/src
+
+$ cd ldlidar_ros_ws/src
+
+$ git clone  https://github.com/ldrobotSensorTeam/ldlidar_stl_ros.git
+# or
+$ git clone  https://gitee.com/ldrobotSensorTeam/ldlidar_stl_ros.git
+```
+## step 1: system setup
+- Connect the LiDAR to your system motherboard via an onboard serial port or usB-to-serial module (for example, CP2102 module).
+
+- Set the -x permission for the serial port device mounted by the radar in the system (for example, /dev/ttyUSB0)
+
+  - In actual use, the LiDAR can be set according to the actual mounted status of your system, you can use 'ls -l /dev' command to view.
+
+``` bash
+$ cd ~/ldlidar_ros_ws
+
+$ sudo chmod 777 /dev/ttyUSB0
+```
+- Modify the `port_name` value in the Lanuch file corresponding to the radar product model under `launch/`, using `ld06.launch` as an example, as shown below.
+
+``` xml
+<launch>
+ <node name="LD06" pkg="ldlidar_stl_ros" type="ldlidar_stl_ros_node" output="screen" >
+ <param name="product_name" value="LDLiDAR_LD06"/>
+ <param name="topic_name" value="LiDAR/LD06"/>
+ <param name="port_name" value ="/dev/ttyUSB0"/>
+ <param name="frame_id" value="lidar_frame"/>
+ </node>
+</launch>
+```
+## step 2: build
+
+Run the following command in the directory where the readme file resides.
+
+```bash
+catkin_make
+```
+## step 3: run
+
+```bash
+source devel/setup.bash
+```
+- The product is LDROBOT LiDAR LD06
+
+  ``` bash
+  roslaunch ldlidar_stl_ros ld06.launch
+  ```
+- The product is LDROBOT LiDAR LD19
+
+  ``` bash
+  roslaunch ldlidar_stl_ros ld19.launch
+  ```
+## step 3: test
+
+> The code was tested under ubuntu16.04 ROS kinetic、ubuntu18.04 ROS melodic、ubuntu20.04 ROS noetic, using rviz visualization.
+
+- new a terminal (Ctrl + Alt + T) and use Rviz2 tool,open the `ldlidar.rviz` file below the rviz folder of the readme file directory
+```bash
+rosrun rviz rviz
+```
+
+| Product:          | Fixed Frame: | Topic:        |
+| ------------------ | ------------ | ------------- |
+| LDROBOT LiDAR LD06 | lidar_frame  | /LiDAR/LD06   |
+| LDROBOT LiDAR LD19 | lidar_frame  | /LiDAR/LD19   |
---
+++ src/main.cpp
@@ -0,0 +1,112 @@
+/**
+ * @file main.cpp
+ * @author LDRobot (marketing1@ldrobot.com)
+ * @brief  main process App
+ *         This code is only applicable to LDROBOT LiDAR LD06 products 
+ * sold by Shenzhen LDROBOT Co., LTD    
+ * @version 0.1
+ * @date 2021-10-28
+ *
+ * @copyright Copyright (c) 2021  SHENZHEN LDROBOT CO., LTD. All rights
+ * reserved.
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License in the file LICENSE
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <ros/ros.h>
+#include <stdio.h>
+
+#include <iostream>
+#include <string>
+
+#include "cmd_interface_linux.h"
+#include "lipkg.h"
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "ldldiar_publisher");
+  ros::NodeHandle nh;  // create a ROS Node
+  ros::NodeHandle n("~");
+  std::string product_name;
+	std::string topic_name;
+	std::string port_name;
+	std::string frame_id;
+
+  n.getParam("product_name", product_name);
+	n.getParam("topic_name", topic_name);
+	n.getParam("port_name", port_name);
+	n.getParam("frame_id", frame_id);
+  
+  // std::cout << "[ldrobot] SDK Pack Version is " << "v2.2.1" << std::endl;
+	// std::cout << "[ldrobot] "
+  //           << " <topic_name>: " << topic_name
+  //           << " <port_name>: "  << port_name
+  //           << " <frame_id>: "   << frame_id << std::endl;
+  ROS_INFO("[ldrobot] SDK Pack Version is v2.2.1");
+  ROS_INFO("[ldrobot] <product_name>: %s,<topic_name>: %s,<port_name>: %s,<frame_id>: %s", 
+    product_name.c_str(), topic_name.c_str(), port_name.c_str(), frame_id.c_str());
+
+
+  LiPkg *lidar = new LiPkg(frame_id);
+  CmdInterfaceLinux cmd_port;
+
+  if (port_name.empty()) {
+    // std::cout << "[ldrobot] Can't find LiDAR LD06 device" << std::endl;
+    ROS_ERROR("[ldrobot] Can't find %s device", product_name.c_str());
+    exit(EXIT_FAILURE);
+  } else {
+    // std::cout << "[ldrobot] FOUND LiDAR_LD06" << std::endl;
+    ROS_INFO("[ldrobot] FOUND %s device", product_name.c_str());
+    cmd_port.SetReadCallback([&lidar](const char *byte, size_t len) {
+      if (lidar->Parse((uint8_t *)byte, len)) {
+        lidar->AssemblePacket();
+      }
+    });
+  }
+
+  if (cmd_port.Open(port_name)) {
+    // std::cout << "[ldrobot] open LiDAR_LD06 device  " << port_name  << " success!"<< std::endl;
+    ROS_INFO("[ldrobot] open %s device %s is success!", product_name.c_str(), port_name.c_str());
+  }else {
+    // std::cout << "[ldrobot] open LiDAR_LD06 device  " << port_name << " fail!"<< std::endl;
+    ROS_INFO("[ldrobot] open %s device %s is fail!", product_name.c_str(), port_name.c_str());
+    exit(EXIT_FAILURE);
+  }
+    
+
+  ros::Publisher lidar_pub = nh.advertise<sensor_msgs::LaserScan>(topic_name, 10);  // create a ROS topic
+  
+  ros::Rate r(10); //10hz
+
+  while (ros::ok()) {
+    if (lidar->IsFrameReady()) {
+      lidar_pub.publish(lidar->GetLaserScan());  // Fixed Frame:  lidar_frame
+      lidar->ResetFrameReady();
+#if 0 
+			sensor_msgs::LaserScan data = lidar->GetLaserScan();
+			unsigned int lens = (data.angle_max - data.angle_min) / data.angle_increment;  
+			std::cout << "[ldrobot] current_speed: " << lidar->GetSpeed() << " " 
+			          << "len: " << lens << " "
+					  << "angle_min: " << RADIAN_TO_ANGLED(data.angle_min) << " "
+					  << "angle_max: " << RADIAN_TO_ANGLED(data.angle_max) << std::endl; 
+			std::cout << "----------------------------" << std::endl;
+			for (unsigned int i = 0; i < lens; i++)
+			{
+				std::cout << "[ldrobot] range: " <<  data.ranges[i] << " " 
+						  << "intensites: " <<  data.intensities[i] << std::endl;
+			}
+			std::cout << "----------------------------" << std::endl;
+#endif
+    }
+    r.sleep();
+  }
+  return 0;
+}
+
+/********************* (C) COPYRIGHT SHENZHEN LDROBOT CO., LTD *******END OF
+ * FILE ********/
