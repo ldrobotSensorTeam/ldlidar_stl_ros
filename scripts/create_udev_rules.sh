27dcfdccf95e862abd2a962d01842cc4e3968ae2
Daisuke Sato <tiryoh@gmail.com>
2022-09-19 17:52:08 +0900

    Support catkin_make_isolated

--- scripts/create_udev_rules.sh
+++ scripts/create_udev_rules.sh
@@ -3,7 +3,7 @@
 echo "remap the device serial port(ttyUSBX) to  ldlidar"
 echo "ldlidar usb connection as /dev/ldlidar , check it using the command : ls -l /dev|grep ttyUSB"
 echo "start copy ldlidar.rules to  /etc/udev/rules.d/"
-sudo cp ./ldlidar.rules  /etc/udev/rules.d
+sudo cp `rospack find ldlidar_stl_ros`/rules/ldlidar.rules /etc/udev/rules.d/
 echo " "
 echo "Restarting udev"
 echo ""
